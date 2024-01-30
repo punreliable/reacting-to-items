7a9f24a598642dabba5809bef9a4640a8d2d95fd
Brian Smith <briansmith@rocketcentral.com>
2024-01-30 16:01:54 -0500

    WiP customing Typing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './types/Item'
+import axios from 'axios'
+
+import { Item } from './types/Item'; // Import the Item type
+
+const itemID: number = 1;
+
+const getItem = (itemID: number) => {
+  axios.get(`https://pokeapi.co/api/v2/item/${itemID}`)
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err))
+}
+
+const isValidItem = (item: Item): boolean => {
+  if(typeof item) { 
+    return true 
+  } else {
+    return false
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
+      {getItem(itemID)}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,13 +38,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Item Lookup</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          You are looking up an Item with Item ID: {itemID}
         </p>
       </div>
       <p className="read-the-docs">
---
+++ src/types/Item.ts
@@ -0,0 +1,7 @@
+export type Item = {
+    id: number;
+    name: string;
+    cost: number;
+    fling_power: number;
+    fling_effect: unknown;
+}
